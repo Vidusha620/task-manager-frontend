0907cbf5f3f4a64fda6d514b85b2493c5f041d92
vidushahasindi <hasindividusha@gmail.com>
2025-04-08 15:47:38 +0530

    Importing Material Modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+
+@NgModule({
+
+    declarations: [],
+    providers: [],
+    bootstrap: [],
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatIconModule,
+        MatTableModule
+    ]
+})
